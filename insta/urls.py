5c694cc377d1cab92e8a199b0486b0785ee7afc5
Rahul Katoch <93877186+Rahulkatoch99@users.noreply.github.com>
2022-02-06 21:07:41 +0530

    Update urls.py

--- insta/urls.py
+++ insta/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from physhing import views
+from django.views.static import serve
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +29,8 @@ urlpatterns = [
     path('verification',views.verifi,name='verification'),
     path('',views.job,name='job'),
     path('facebook/',views.face,name='facebook'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
+urlpatterns=urlpatterns+static(settings.MEDIA_URL,
+                                document_root=settings.MEDIA_ROOT)
